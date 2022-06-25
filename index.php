1356acfc11b7103340f6a4c65d58981083e038d5
ganesh <100138873+ganesh-shinde-gs@users.noreply.github.com>
2022-06-25 13:00:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
+<?php
+session_start();
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	if (!empty($name=="ganesh shinde")) {
+		$_SESSION['name'] = "ganesh shinde";
+		header('location:wecome.php');
+		die();
+	}
+}
+?>
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
 
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+<form method="post">
+
+	<label for="fname">First Name</label>
+    <input type="text" id="fname" name="name" placeholder="Your name..">
+    <input type="submit" name="submit" value="Click" />
+</form>
